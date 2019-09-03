fbc7c4f7f65bf185b0fbdad99762697d91894868
louie.kim <hyeok0724@naver.com>
2019-09-04 08:46:18 +0900

    190904 0845

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     ser = serial.Serial('/dev/ttyUSB0', 9600)
 
     data = ser.read()
+    print(data)
     print("compete read")
     ser.write(data)
     print("write Data")
