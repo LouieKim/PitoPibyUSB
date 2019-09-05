ef2723675141f6faf91bef2cbc267c3ce8e174d4
louie.kim <hyeok0724@naver.com>
2019-09-05 11:36:30 +0900

    190905 1136

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import serial
 
+def write_data(data):
+    f = open("test_txt.txt", 'w')
+
+    for i in range(11, 21):
+        data = "%d Line\n" % i
+        f.write(data)
+
+    f.close()
+
 if __name__ == "__main__":
     print("Start\n")
     ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-    data = ser.read()
-    print(data)
-    print("compete read")
-    ser.write(data)
-    print("write Data")
-    ser.close()
+    while(1):
+        data = ser.read()
+        print(\n data)
+        print("compete read")
+        write_data(data)
+        print("write Data")
+        ser.close()
