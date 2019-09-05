69a939b9f0edde86582c40550cc415bd0c56a22f
louie.kim <hyeok0724@naver.com>
2019-09-05 14:07:33 +0900

    190905 1346

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import curses
 def write_data(data):
     f = open("test_txt.txt", 'a')
     f.write(data)
+    print("Input Data: %s",%*data)
+
     f.close()
 
 if __name__ == "__main__":
@@ -11,7 +13,7 @@ if __name__ == "__main__":
     ser = serial.Serial('/dev/ttyUSB0', 9600)
 
     while(1):
-        data = ser.read()
+        data = ser.read(1)
         print(data)
         print("compete read")
         write_data(str(data, "utf-8"))
