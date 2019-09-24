f4566762739cb1b781e2fce9164c87ca749c9753
louie.kim <hyeok0724@naver.com>
2019-09-24 11:43:17 +0900

    190924 1143

--- test_folder/chat_client_Test.py
+++ test_folder/chat_client_Test.py
@@ -1,35 +1,40 @@
 import socket
 from threading import Thread
+import time
 
 HOST ='192.168.10.4'
 PORT = 9009
 
 def rcvMsg(sock):
-    while True:
-        try:
-            data = sock.recv(1024)
-            if not data:
-                break
-        print(str(data))
+   while True:
+      try:
+         data = sock.recv(1024)
+         if not data:
+            break
 
-        hlist = [hxstr[i:i+2] for i in range(0,len(hxstr),2)]
+         print(str(data))
+         hlist=[data[i:i+1] for i in range(0,len(data),1)]
 
-        print(hlist)
+         print(hlist)
+         #print(data.decode())
 
-    except:
-        pass
+      except:
+         pass
 
 def runChat():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((HOST, PORT))
-        t = Thread(target=rcvMsg, args=(sock,))
-        t.daemon = True
-        t.start()
-        while True:
-            msg = input()
-            if msg =='/quit':
-                sock.send(msg.encode())
-                break
+   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+      sock.connect((HOST, PORT))
+      t = Thread(target=rcvMsg, args=(sock,))
+      t.daemon = True
+      t.start()
+
+      while True:
+         msg = b'0301060879128753'
+         if msg =='/quit':
             sock.send(msg.encode())
+            break
+         sock.send(msg)
+         time.sleep(1)
+         #sock.send(msg.encode())
 
 runChat()
